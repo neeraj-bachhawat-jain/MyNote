0f0ee80e9de50602e2f59f329bb09b4ed5e332fb
Neeraj Jain <neeraj2003.jain@gmail.com>
2026-04-15 11:11:48 +0530

    Restrict CORS to specific origin

--- Backend/index.mjs
+++ Backend/index.mjs
@@ -6,7 +6,11 @@ import cors from 'cors';
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: [
+        "https://mynote-onq7.onrender.com"
+    ]
+}));
 mongoose.connect(URI).then(()=>console.log("database connected successfully")
 ).catch((err)=>console.log(err)); 
 app.use('/', router);
